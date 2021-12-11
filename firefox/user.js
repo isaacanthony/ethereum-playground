46afce340d85d175f811f4c209fad8b589cd112d
Isaac <ianthony001@gmail.com>
2021-12-11 09:07:24 -0500

    Add do not track config to firefox.

--- firefox/user.js
+++ firefox/user.js
@@ -1,1 +1,8 @@
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.search.hiddenOneOffs", "Google,Amazon.com,Bing,DuckDuckGo,Wikipedia (en)");
 user_pref("browser.startup.homepage", "https://addons.mozilla.org/en-US/firefox/addon/ether-metamask");
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("privacy.donottrackheader.enabled", true);
