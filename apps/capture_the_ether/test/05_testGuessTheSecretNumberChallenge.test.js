fc566c0e2fcb1fd3b4f9c46d0175d8bc62c6c540
Isaac <ianthony001@gmail.com>
2021-12-27 13:22:42 -0500

    Add Capture the Ether Chp5.

---
+++ apps/capture_the_ether/test/05_testGuessTheSecretNumberChallenge.test.js
@@ -0,0 +1,59 @@
+const GuessTheSecretNumberChallenge = artifacts.require(
+  'GuessTheSecretNumberChallenge'
+);
+
+contract('GuessTheSecretNumberChallenge', (accounts) => {
+  let target;
+
+  beforeEach(async () => {
+    target = await GuessTheSecretNumberChallenge.new({
+      value: web3.utils.toWei('1', 'ether'),
+    });
+  });
+
+  describe('#guess', async () => {
+    it('rewards the correct guess', async () => {
+      const oldBalance = web3.utils.fromWei(
+        await web3.eth.getBalance(accounts[0]),
+        'ether',
+      );
+
+      const targetHash = `
+        0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365
+      `.trim();
+
+      let guessInt;
+      let guessHash;
+
+      for (guessInt = 0; guessInt < 1e3; guessInt++) {
+        guessHash = web3.utils.soliditySha3({
+          type: 'uint8',
+          value: guessInt.toString(),
+        });
+
+        if (guessHash === targetHash) {
+          break
+        }
+      }
+
+      assert.isFalse(await target.isComplete());
+
+      await target.guess(guessInt, {
+        from: accounts[0],
+        value: web3.utils.toWei('1', 'ether'),
+      });
+
+      assert.isTrue(await target.isComplete());
+
+      const newBalance = web3.utils.fromWei(
+        await web3.eth.getBalance(accounts[0]),
+        'ether',
+      );
+
+      assert.equal(
+        Math.floor(oldBalance) + 1,
+        Math.floor(newBalance),
+      );
+    });
+  });
+});
